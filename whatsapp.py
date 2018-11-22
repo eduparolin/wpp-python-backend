dabf692cb63a0b861bcfd1c73c612173d504ac29
eduardo.parolin <eduardo.parolin@tuntscorp.com>
2018-11-22 16:36:54 -0200

    fix

--- whatsapp.py
+++ whatsapp.py
@@ -21,7 +21,6 @@ from Crypto.Hash import SHA256;
 import hashlib;
 import hmac;
 import traceback;
-import ssl;
 
 import websocket;
 import curve25519;
@@ -251,7 +250,6 @@ class WhatsAppWebClient:
                                                on_open=lambda ws: self.onOpen(ws),
                                                on_close=lambda ws: self.onClose(ws),
                                                header={"Origin: https://web.whatsapp.com"});
-	self.activeWs.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE});
 
         self.websocketThread = Thread(target=self.activeWs.run_forever);
         self.websocketThread.daemon = True;
